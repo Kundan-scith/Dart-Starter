983e9cb29c11432dcf217582f032d41af1aa215b
Kundan Kumar <85442986+Kundan-scith@users.noreply.github.com>
2022-09-02 21:21:21 +0530

    Create Logics.dart

---
+++ Logics.dart
@@ -0,0 +1,24 @@
+void main() {
+  //Logics
+  
+  //If
+  var number = 5;
+  if(number == 5){
+    print("The number is 5!");
+  }
+  
+  //If-else
+  if(number != 5){
+    print("The number is not 5!");
+  }else{
+    print("Hello world, The number is 5!");
+  }
+  
+  //If Else If
+  var num = 10;
+  if(num==3){
+    print("The number is 3!");
+  }else if(num==10){
+    print("Hello World");
+  }
+}
