01f6dd6565f504f162bdc9feec89d38bd89e4669
Kundan Kumar <85442986+Kundan-scith@users.noreply.github.com>
2022-09-02 20:14:08 +0530

    Create dataType.dart

---
+++ dataType.dart
@@ -0,0 +1,24 @@
+void main() {
+  //Data Types
+  
+  //Strings
+  String myCode = "Hello World";
+  print("String: $myCode");
+  
+  //Integers
+  int math = 10;
+  print("Integer: $math");
+  
+  //Doubles
+  double decimal = 3.14;
+  print("Double: $decimal");
+    
+  //Booleans
+  bool status = false;
+  print("Boolean: $status"); 
+  
+  //Dynamic
+  dynamic fullName = "John Walker";
+  print("Dynamic: $fullName");
+  
+}
