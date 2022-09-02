1d06fb822900ec976ec1ba72ad10d467d5aa59a2
Kundan Kumar <85442986+Kundan-scith@users.noreply.github.com>
2022-09-02 19:58:02 +0530

    Create varibles.dart

---
+++ varibles.dart
@@ -0,0 +1,32 @@
+void main() {
+  //Variables
+  
+  //String
+  var firstName = "first";
+  String secondName = "second";
+  print(firstName);
+  print(secondName);
+  
+  //Integer
+  var x = 10;
+  int y = 10;
+  print(x);
+  print(y);
+  
+  //Dynamic(if datatype is not clear)
+  dynamic name = "John Carter";
+  dynamic number = 20;
+  print(name);
+  print(number);
+  
+  //Constant and Final
+  const String fullName = "Dart";
+  final String lastName = "Den";
+  print(fullName);
+  print(lastName);
+  
+  var myCode;
+  print(myCode);
+  myCode = "HELLO WORLD";
+  print(myCode);
+}
