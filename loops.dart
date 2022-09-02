17f5351a2cfdadbdb76bfac8a60404c142aafd4b
Kundan Kumar <85442986+Kundan-scith@users.noreply.github.com>
2022-09-02 21:12:51 +0530

    Create loops.dart

---
+++ loops.dart
@@ -0,0 +1,20 @@
+void main() {
+  // For Loop
+  for (int i = 1; i <= 5; i++) {
+    print(i);
+  }
+  
+  //For In Loop
+  var names = ["John", "Rick", "Tom", "Jerry", "Donald"];
+  print(names);
+  for (var name in names){
+    print(name);
+  }
+  
+  //While Loop
+  var num = 5;
+  while(num >=1){
+    print(num);
+    num--;
+  }
+}
